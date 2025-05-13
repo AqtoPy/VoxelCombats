9097d3d4ff16054664de7ffc5f8afe4937dcf012
AqtoPy <rekikreki66@gmail.com>
2025-05-13 18:19:27 +0300

    Update WeaponManager.gd

--- WeaponManager.gd
+++ WeaponManager.gd
@@ -1,144 +1,154 @@
 extends Node3D
-class_name WeaponManager
 
-signal weapon_changed(weapon_data: Dictionary)
-signal ammo_updated(current: int, reserve: int)
-signal skin_updated(weapon_id: String, skin_id: String)
-signal zoom_state_changed(is_zoomed: bool)
+signal weapon_changed
+signal update_ammo
+signal update_weapon_stack
+signal hit_successful
+signal add_signal_to_hud
+signal connect_weapon_to_hud
 
+@export var animation_player: AnimationPlayer
+@export var melee_hitbox: ShapeCast3D
 @export var max_weapons: int = 3
 @export var hands_scene: PackedScene
-@export var default_fov: float = 75.0
+@export var crosshair_texture: Texture2D
 
-var current_weapon: Node3D = null
-var weapons: Array = []
-var current_slot: int = 0
-var is_reloading: bool = false
-var is_zoomed: bool = false
-var current_skin: String = "default"
+@onready var bullet_point = get_node("%BulletPoint")
+@onready var debug_bullet = preload("res://Player_Controller/Spawnable_Objects/hit_debug.tscn")
+@onready var crosshair = preload("res://UI/Crosshair.tscn")
 
-var weapon_slots: Dictionary = {
-    "primary": null,
-    "secondary": null,
-    "melee": null
+var hands_instance: Node3D
+var current_crosshair: Control
+var next_weapon: WeaponSlot
+var spray_profiles: Dictionary = {}
+var _count = 0
+var shot_tween
+
+# Магазин оружия (пример)
+var weapon_shop = {
+    "pistol": {"cost": 100, "scene": preload("res://Weapons/Pistol.tscn")},
+    "rifle": {"cost": 300, "scene": preload("res://Weapons/Rifle.tscn")},
+    "shotgun": {"cost": 500, "scene": preload("res://Weapons/Shotgun.tscn")}
 }
 
-func _ready():
-    load_player_loadout()
-    spawn_hands()
-    equip_weapon("primary")
+@export var weapon_stack: Array[WeaponSlot] # Массив оружия у игрока
+var current_weapon_slot: WeaponSlot = null
+var available_weapons: Array[WeaponSlot] = [] # Все доступные оружия
 
-func spawn_hands():
-    if hands_scene:
-        var hands = hands_scene.instantiate()
-        add_child(hands)
-
-func load_player_loadout():
-    var loadout = PlayerData.get_current_loadout()
+func _ready() -> void:
+    # Инициализация рук
+    hands_instance = hands_scene.instantiate()
+    add_child(hands_instance)
     
-    for slot in loadout.weapons:
-        var weapon_data = loadout.weapons[slot]
-        var weapon = weapon_data.weapon_scene.instantiate()
-        
-        # Применяем сохраненный скин
-        if weapon_data.has("skin"):
-            weapon.apply_skin(weapon_data.skin)
-            current_skin = weapon_data.skin
-        
-        weapon_slots[slot] = weapon
-        add_child(weapon)
-        weapon.hide()
-
-func equip_weapon(slot: String):
-    if weapon_slots[slot] == null:
-        return
+    # Инициализация прицела
+    current_crosshair = crosshair.instantiate()
+    get_tree().root.get_node("HUD").add_child(current_crosshair)
     
-    if current_weapon:
-        unequip_current_weapon()
-    
-    current_weapon = weapon_slots[slot]
-    current_weapon.show()
-    
-    var weapon_data = {
-        "name": current_weapon.weapon_name,
-        "slot": slot,
-        "skin": current_skin
-    }
-    
-    emit_signal("weapon_changed", weapon_data)
-    update_ammo_display()
-
-func unequip_current_weapon():
-    if current_weapon:
-        current_weapon.hide()
-
-func update_ammo_display():
-    if current_weapon:
-        emit_signal("ammo_updated", 
-            current_weapon.current_ammo, 
-            current_weapon.reserve_ammo)
-
-func _input(event):
-    handle_weapon_input(event)
-    handle_zoom_input(event)
-
-func handle_weapon_input(event):
-    if event.is_action_pressed("slot1"):
-        equip_weapon("primary")
-    elif event.is_action_pressed("slot2"):
-        equip_weapon("secondary")
-    elif event.is_action_pressed("slot3"):
-        equip_weapon("melee")
-    elif event.is_action_pressed("reload"):
-        start_reload()
-    elif event.is_action_pressed("shoot"):
-        start_firing()
-    elif event.is_action_released("shoot"):
-        stop_firing()
-
-func handle_zoom_input(event):
-    if event.is_action_pressed("zoom") and current_weapon.can_zoom:
-        toggle_zoom()
-
-func toggle_zoom():
-    is_zoomed = !is_zoomed
-    if is_zoomed:
-        get_viewport().get_camera_3d().fov = current_weapon.zoom_fov
+    if weapon_stack.is_empty():
+        push_error("Weapon Stack is empty, please populate with weapons")
     else:
-        get_viewport().get_camera_3d().fov = default_fov
-    emit_signal("zoom_state_changed", is_zoomed)
+        animation_player.animation_finished.connect(_on_animation_finished)
+        for i in weapon_stack:
+            initialize_weapon(i)
+        current_weapon_slot = weapon_stack[0]
+        if check_valid_weapon_slot():
+            enter()
+            update_weapon_stack.emit(weapon_stack)
 
-func start_firing():
-    if can_fire():
-        current_weapon.fire()
-        update_ammo_display()
-
-func can_fire() -> bool:
-    return !is_reloading && current_weapon.current_ammo > 0
-
-func start_reload():
-    if can_reload():
-        is_reloading = true
-        current_weapon.reload()
-        await get_tree().create_timer(current_weapon.reload_time).timeout
-        finish_reload()
-
-func can_reload() -> bool:
-    return !is_reloading && current_weapon.reserve_ammo > 0
-
-func finish_reload():
-    var ammo_needed = current_weapon.magazine_size - current_weapon.current_ammo
-    var ammo_from_reserve = min(ammo_needed, current_weapon.reserve_ammo)
+func initialize_weapon(_weapon_slot: WeaponSlot):
+    if !_weapon_slot or !_weapon_slot.weapon:
+        return
+        
+    # Загружаем сцену оружия
+    var weapon_scene = _weapon_slot.weapon.weapon_scene.instantiate()
+    add_child(weapon_scene)
+    _weapon_slot.weapon_instance = weapon_scene
     
-    current_weapon.current_ammo += ammo_from_reserve
-    current_weapon.reserve_ammo -= ammo_from_reserve
+    if _weapon_slot.weapon.weapon_spray:
+        spray_profiles[_weapon_slot.weapon.weapon_name] = _weapon_slot.weapon.weapon_spray.instantiate()
     
-    is_reloading = false
-    update_ammo_display()
+    connect_weapon_to_hud.emit(_weapon_slot.weapon)
+    weapon_scene.visible = false # Сначала скрываем
 
-func change_weapon_skin(skin_id: String):
-    if current_weapon:
-        current_weapon.apply_skin(skin_id)
-        current_skin = skin_id
-        emit_signal("skin_updated", current_weapon.weapon_id, skin_id)
-        PlayerData.set_weapon_skin(current_weapon.weapon_id, skin_id)
+func enter() -> void:
+    if current_weapon_slot.weapon_instance:
+        # Показываем текущее оружие и скрываем остальные
+        for weapon in weapon_stack:
+            if weapon.weapon_instance:
+                weapon.weapon_instance.visible = (weapon == current_weapon_slot)
+        
+        current_weapon_slot.weapon_instance.visible = true
+        animation_player.queue(current_weapon_slot.weapon.pick_up_animation)
+        weapon_changed.emit(current_weapon_slot.weapon.weapon_name)
+        update_ammo.emit([current_weapon_slot.current_ammo, current_weapon_slot.reserve_ammo])
+        
+        # Обновляем прицел
+        update_crosshair(current_weapon_slot.weapon.crosshair_type)
+
+func exit(_next_weapon: WeaponSlot) -> void:
+    if _next_weapon != current_weapon_slot:
+        if animation_player.get_current_animation() != current_weapon_slot.weapon.change_animation:
+            animation_player.queue(current_weapon_slot.weapon.change_animation)
+            next_weapon = _next_weapon
+
+func update_crosshair(type: String) -> void:
+    match type:
+        "dot":
+            current_crosshair.set_texture(crosshair_texture, Color.WHITE, 10)
+        "circle":
+            current_crosshair.set_texture(crosshair_texture, Color.WHITE, 20)
+        "rifle":
+            current_crosshair.set_complex_crosshair()
+        _:
+            current_crosshair.set_texture(crosshair_texture, Color.WHITE, 10)
+
+# Покупка оружия в магазине
+func buy_weapon(weapon_name: String, player_money: int) -> bool:
+    if weapon_shop.has(weapon_name):
+        var weapon_data = weapon_shop[weapon_name]
+        if player_money >= weapon_data["cost"]:
+            # Создаем новый слот оружия
+            var new_slot = WeaponSlot.new()
+            new_slot.weapon = weapon_data["scene"].weapon_resource
+            
+            # Добавляем в доступное оружие
+            available_weapons.append(new_slot)
+            return true
+    return false
+
+# Выбор оружия из меню
+func select_weapon_from_menu(weapon_slot: WeaponSlot) -> void:
+    if weapon_stack.size() >= max_weapons:
+        # Заменяем первое оружие
+        weapon_stack[0] = weapon_slot
+    else:
+        weapon_stack.append(weapon_slot)
+    
+    initialize_weapon(weapon_slot)
+    exit(weapon_slot)
+
+# Остальные функции (shoot, reload, melee и т.д.) остаются такими же, как в оригинальном скрипте,
+# но с учетом того, что оружие теперь отдельные сцены
+
+func _on_animation_finished(anim_name):
+    if anim_name == current_weapon_slot.weapon.shoot_animation:
+        if current_weapon_slot.weapon.auto_fire == true:
+            if Input.is_action_pressed("Shoot"):
+                shoot()
+
+    if anim_name == current_weapon_slot.weapon.change_animation:
+        change_weapon(next_weapon)
+    
+    if anim_name == current_weapon_slot.weapon.reload_animation:
+        if !current_weapon_slot.weapon.incremental_reload:
+            calculate_reload()
+
+func change_weapon(weapon_slot: WeaponSlot) -> void:
+    current_weapon_slot = weapon_slot
+    next_weapon = null
+    enter()
+
+func _process(delta):
+    # Обновление позиции оружия относительно рук
+    if current_weapon_slot and current_weapon_slot.weapon_instance:
+        current_weapon_slot.weapon_instance.global_transform = hands_instance.get_node("WeaponPosition").global_transform
