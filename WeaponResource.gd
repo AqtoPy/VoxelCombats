fce3450e748fe2373f1f5130d55294c71b7357f8
AqtoPy <rekikreki66@gmail.com>
2025-05-13 17:58:49 +0300

    Update WeaponResource.gd

--- WeaponResource.gd
+++ WeaponResource.gd
@@ -1,32 +1,61 @@
+@icon("res://Player_Controller/scripts/Weapon_State_Machine/weapon_resource_icon.svg")
 extends Resource
 class_name WeaponResource
 
+signal update_overlay
+signal zoom_changed(is_zoomed: bool)
+
+# Группа Weapon Base
 @export_category("Base Settings")
 @export var weapon_id: String
 @export var weapon_name: String
-@export var slot_type: int  # WeaponManager.WeaponSlot
+@export var slot_type: String # "primary", "secondary", "melee", "grenade"
+@export var unlock_price: int = 0
+@export var unlocked: bool = true
 
-@export_category("Combat Settings")
+# Групка Animations
+@export_category("Weapon Animations")
+@export var pick_up_animation: String
+@export var shoot_animation: String
+@export var reload_animation: String
+@export var change_animation: String
+@export var drop_animation: String
+@export var out_of_ammo_animation: String
+@export var melee_animation: String
+@export var inspect_animation: String
+@export var arms_animations: Dictionary = {
+    "reload": "reload_rifle",
+    "shoot": "shoot_rifle"
+}
+
+# Группа Stats
+@export_category("Weapon Stats")
 @export var damage: int
 @export var fire_rate: float
 @export var magazine_size: int
-@export var max_reserve_ammo: int
+@export var max_ammo: int
 @export var reload_time: float
+@export var is_automatic: bool
+@export var fire_range: int
+@export var melee_damage: float
+@export var can_zoom: bool
+@export var zoom_fov: float = 30.0
 
+# Группа Visuals
 @export_category("Visual Settings")
-@export var weapon_model: PackedScene
-@export var texture_albedo: Texture2D
+@export var weapon_scene: PackedScene
+@export var default_skin: Texture2D
 @export var has_scope: bool
 @export var scope_texture: Texture2D
+@export var viewmodel_position: Vector3
+@export var viewmodel_rotation: Vector3
 
-@export_category("Animations")
-@export var hands_animations: Dictionary = {
-    "draw": "draw_rifle",
-    "fire": "fire_rifle",
-    "reload": "reload_rifle"
-}
-
-@export_category("Ballistics")
-@export var bullet_scene: PackedScene
-@export var bullet_speed: float
-@export var bullet_drop: float
+# Группа Behavior
+@export_category("Weapon Behavior")
+@export var can_be_dropped: bool
+@export var weapon_drop: PackedScene
+@export var weapon_spray: PackedScene
+@export var projectile_to_load: PackedScene
+@export var incremental_reload: bool
+@export var bullet_count: int = 1 # Для дробовиков
+@export var spread_angle: float = 0.0
